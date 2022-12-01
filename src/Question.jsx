26cc79b733433b8b4b3bcc251b6750d83dc95c68
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-02 00:27:08 +0530

    refactored the code

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,22 +1,18 @@
 import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Question = ({ title, info }) => {
-  const [display, setDisplay] = useState("none");
+  const [showInfo, setShowInfo] = useState(false);
 
   return (
     <div className="question">
       <header>
         <h4>{title}</h4>
-        <button
-          className="btn"
-          onClick={() => {
-            display === "none" ? setDisplay("block") : setDisplay("none");
-          }}
-        >
-          {display === "none" ? "+" : "-"}
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      <p style={{ display }}>{info}</p>
+      {showInfo && <p>{info}</p>}
     </div>
   );
 };
